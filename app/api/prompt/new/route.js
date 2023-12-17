08527ba92edd16d1fad0a616a2b0df5423dcab92
vector-10 <duzieblaise10@gmail.com>
2023-12-17 06:02:09 +0100

    update

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,15 +1,21 @@
 import { connectToDB } from "@utils/database";
-import Prompt from
+import Prompt from "@models/prompt";
  
  export const POST = async (req, res) => {
-
     const { userId, prompt, tag } = await req.json();
 
     try {
         await connectToDB();
+        const newPrompt = await Prompt.create({
+            creator: userId,
+            prompt,
+            tag,
+        });
+
+        return new Response(JSON.stringify(newPrompt), {
+            status: 201
+        })
     } catch (error) {
-        
+      console.log(error);
     }
-
-
  }
